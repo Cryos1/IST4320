a6515153b6d817db9d77d138582bcfb3a0738486
Cryos1 <006479131@coyote.csusb.edu>
2024-09-29 20:21:19 -0700

    First upload of Name in Lights Python File

---
+++ NameinLights.py
@@ -0,0 +1,11 @@
+Python 3.11.1 (v3.11.1:a7a450f84a, Dec  6 2022, 15:24:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Sebastian Torres, Name in Lights, Ver 1.0, 2/19/23
+... # This program draws a capital S' in *
+... print("*****\n*    \n*****\n    *\n*****\n")
+*****
+*    
+*****
+    *
+*****
+
